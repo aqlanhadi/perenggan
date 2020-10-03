11df2f0763d08748fb50ef3814a756aa3d5b2a68
seabil <syabilhud98@gmail.com>
2020-10-03 12:39:17 +0100

    Added svg Perenggan

--- src/App.js
+++ src/App.js
@@ -4,21 +4,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and dave to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Perenggan">
+      <header className="Perenggan-header"></header>
     </div>
   );
 }
