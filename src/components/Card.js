411e5d4b15d16b922b5d741669ee8d7ea5110b46
Aqlan Nor Azman <aqlanhadi@gmail.com>
2020-10-03 19:30:40 +0800

    added tailwind

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="max-w-sm rounded overflow-hidden shadoq-lg">
+            <img className="" src={require('../assets/IMG_4114.jpg')} alt="Display" />
+            <div className="">
+              <div className="">
+                Aqlan Nor Azman
+              </div>
+              <p className="">
+                Weeeee<span></span>😜
+              </p>
+            </div>
+          </div>
+        )
+    }
+}
+
+export default Header
